984f4ced91059bccfe4bfe9585e4198b7085aeec
nielsv <niels.vandekeybus@tenforce.com>
2014-01-13 14:03:54 +0100

    renamed hooks-core to edcat-plugin-base in poms

--- src/main/java/eu/lod2/edcat/controller/dataset/CreateController.java
+++ src/main/java/eu/lod2/edcat/controller/dataset/CreateController.java
@@ -3,15 +3,10 @@ package eu.lod2.edcat.controller.dataset;
 import eu.lod2.edcat.utils.Catalog;
 import eu.lod2.edcat.utils.Constants;
 import eu.lod2.edcat.utils.SparqlEngine;
-import eu.lod2.hooks.constraints.graph.CycleException;
-import eu.lod2.hooks.handlers.HookHandler;
-import eu.lod2.hooks.handlers.OptionalHookHandler;
 import eu.lod2.hooks.handlers.dcat.AtCreateHandler;
 import eu.lod2.hooks.handlers.dcat.PostCreateHandler;
 import eu.lod2.hooks.handlers.dcat.PreCreateHandler;
-import eu.lod2.hooks.util.ActionAbortException;
 import eu.lod2.hooks.util.HookManager;
-import eu.lod2.hooks.util.MultiImplementedHookException;
 import org.openrdf.model.Model;
 import org.openrdf.model.URI;
 import org.springframework.http.HttpStatus;
@@ -30,6 +25,7 @@ public class CreateController extends DatasetController {
   @RequestMapping(value = ROUTE, method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
   public ResponseEntity<Object> create(HttpServletRequest request) throws Throwable {
     SparqlEngine engine = new SparqlEngine();
+    HookManager.callHook(PreCreateHandler.class, "handlePreCreate", request, engine);
     Catalog catalog = new Catalog(engine, Constants.getURIBase());
     HookManager.callHook(PreCreateHandler.class, "handlePreCreate", catalog, request, engine);
     Model record = catalog.insertDataset(getId());
