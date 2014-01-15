981ae04ba00eb1bb83c624cebd0f411667db45d8
Aad Versteden <aad@knowified.com>
2014-01-15 15:47:27 +0100

    Bulk base of the validation plugin.

--- src/main/java/eu/lod2/edcat/controller/dataset/CreateController.java
+++ src/main/java/eu/lod2/edcat/controller/dataset/CreateController.java
@@ -28,7 +28,7 @@ public class CreateController extends DatasetController {
   @RequestMapping(value = ROUTE, method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
   public ResponseEntity<Object> create(HttpServletRequest request) throws Throwable {
     SparqlEngine engine = new SparqlEngine();
-    Catalog catalog = new Catalog(engine, Constants.getURIBase());
+    Catalog catalog = new Catalog(engine, Constants.DEFAULT_URI);
     String datasetBaseId = getId();
     URI datasetUri = catalog.generateDatasetUri(datasetBaseId);
     HookManager.callHook(PreCreateHandler.class, "handlePreCreate", new PreContext(catalog, request, engine, datasetUri));
