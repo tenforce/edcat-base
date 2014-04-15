29d9c59ea502e47f8a4bcffe19c0f1e24f562669
nielsv <niels.vandekeybus@tenforce.com>
2014-04-15 10:36:16 +0200

    allow cross origin requests on the api

---
+++ src/main/java/eu/lod2/edcat/filter/SimpleCORSFilter.java
@@ -0,0 +1,35 @@
+package eu.lod2.edcat.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * SimpleCORSFilter responds to all requests with certain Access-Control-* headers.
+ * In this case, the headers are set to allow POST, GET, PUT, OPTIONS, or DELETE requests from clients originated from any host.
+ * The results of a preflight request may be cached for up to 1 day. And the request may include an x-requested-with header.
+ * based on https://spring.io/guides/gs/rest-service-cors/
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "86400");
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+    chain.doFilter(req, res);
+  }
+
+  public void init(FilterConfig filterConfig) {}
+
+  public void destroy() {}
+
+}
